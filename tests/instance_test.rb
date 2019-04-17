67f767bd0ced6d307e29fc4f40a4aaa4393bb7f6
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-04-16 17:10:56 -0700

    test(instance) Write test suite for the `Instance` class.

---
+++ tests/instance_test.rb
@@ -0,0 +1,48 @@
+require "prelude"
+
+
+class WasmerTest < Minitest::Test
+  def bytes
+    IO.read File.expand_path("tests.wasm", File.dirname(__FILE__)), mode: "rb"
+  end
+
+  def test_can_construct
+    assert Instance.new self.bytes
+  end
+
+  def test_basic_sum
+    assert_equal 3, Instance.new(self.bytes).exports.sum(1, 2)
+  end
+
+  def test_call_arity_0
+    assert_equal 42, Instance.new(self.bytes).exports.arity_0
+  end
+
+  def test_call_i32_i32
+    assert_equal 7, Instance.new(self.bytes).exports.i32_i32(7)
+  end
+
+  def test_call_i64_i64
+    assert_equal 7, Instance.new(self.bytes).exports.i64_i64(7)
+  end
+
+  def test_call_f32_f32
+    assert_equal 7.0, Instance.new(self.bytes).exports.f32_f32(7.0)
+  end
+
+  def test_call_f64_f64
+    assert_equal 7.0, Instance.new(self.bytes).exports.f64_f64(7.0)
+  end
+
+  def test_call_i32_i64_f32_f64_f64
+    assert_equal 1 + 2 + 3.4 + 5.6, Instance.new(self.bytes).exports.i32_i64_f32_f64_f64(1, 2, 3.4, 5.6).round(6)
+  end
+
+  def test_call_bool_casted_to_i32
+    assert_equal 1, Instance.new(self.bytes).exports.bool_casted_to_i32
+  end
+
+  def test_call_string
+    assert_equal 1048576, Instance.new(self.bytes).exports.string
+  end
+end
--- tests/wasmer_test.rb
+++
@@ -1,7 +0,0 @@
-require "prelude"
-
-class WasmerTest < Minitest::Test
-  def test_it_reverses
-    assert_equal 2, Instance.new().call()
-  end
-end
