139a4d0d04179999e3ae16abee1e8e351cd9efca
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-04-16 17:10:32 -0700

    doc(examples) More idiomatic Ruby.

--- examples/simple.rb
+++ examples/simple.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
 require "wasmer"
 
-file = File.expand_path("simple.wasm", File.dirname(__FILE__))
-bytes = IO.read(file, mode: "rb")
-instance = Instance.new(bytes)
-puts instance.exports.sum(1, 2)
+file = File.expand_path "simple.wasm", File.dirname(__FILE__)
+bytes = IO.read file, mode: "rb"
+instance = Instance.new bytes
+puts instance.exports.sum 1, 2
