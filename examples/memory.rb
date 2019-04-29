c8bfd8fef1ab6ddc9bef4abc9ec9a03e64199d1d
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-04-29 15:53:59 +0200

    doc(examples) Add the `memory` example.

---
+++ examples/memory.rb
@@ -0,0 +1,25 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
+require "wasmer"
+
+file = File.expand_path "memory.wasm", File.dirname(__FILE__)
+bytes = IO.read file, mode: "rb"
+instance = Instance.new bytes
+pointer = instance.exports.return_hello
+
+memory = instance.memory.uint8_view pointer
+nth = 0
+string = ''
+
+while true
+  char = memory.get nth
+
+  if 0 == char
+    break
+  end
+  
+  string += char.chr
+  nth += 1
+end
+
+puts string
