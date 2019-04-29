d37338d4d120ddcfd60eb3934f8be96f27aae463
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-04-29 15:05:41 +0200

    test(memory) Add test cases for `MemoryView`.

--- tests/memory_test.rb
+++ tests/memory_test.rb
@@ -13,7 +13,31 @@ class ModuleTest < Minitest::Test
     assert_instance_of MemoryView, Instance.new(self.bytes).memory.view(7)
   end
 
-  def test_view_length
+  def test_length
     assert_equal 1114112, Instance.new(self.bytes).memory.view.length
+  end
+
+  def test_get
+    memory = Instance.new(self.bytes).memory.view
+    index = 0
+    value = 42
+    memory.set(index, value)
+
+    assert_equal value, memory.get(index)
+  end
+
+  def test_hello_world
+    instance = Instance.new(self.bytes)
+    pointer = instance.exports.string
+    memory = instance.memory.view(pointer)
+    nth = 0
+    string = ''
+
+    while 0 != memory.get(nth)
+      string += memory.get(nth).chr
+      nth += 1
+    end
+
+    assert_equal "Hello, World!", string
   end
 end
