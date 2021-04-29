fec95e4ef885eb37e8e5a8a8ab03601ffe6cc2fb
Ivan Enderlin <ivan@mnt.io>
2021-04-29 16:13:12 +0200

    feat: Create a macro to simplify class/methods definitions.

--- crates/wasmer/src/lib.rs
+++ crates/wasmer/src/lib.rs
@@ -8,79 +8,77 @@ mod types;
 
 use rutie::{Class, Integer, Module, Object};
 
+macro_rules! ruby_define {
+    (in $module:ident
+        $( class ($class_rust_module:path) $class_name:ident {
+           $( @const $constant_name:ident = $constant_value:expr; )*
+           $( $ruby_definition:ident ($method_rust_name:ident) $method_name:expr; )*
+        }; )*
+    ) => {
+        $(
+            {
+                let data_class = Class::from_existing("Object");
+
+                $module
+                    .define_nested_class(stringify!($class_name), Some(&data_class))
+                    .define(|this| {
+                        #[allow(unused_imports)]
+                        use $class_rust_module::*;
+
+                        $(
+                            this.$ruby_definition($method_name, $method_rust_name);
+                        )*
+
+                        $(
+                            this.const_set(stringify!($constant_name), &$constant_value);
+                        )*
+                    });
+            }
+        )*
+    }
+}
+
 #[allow(non_snake_case)]
 #[no_mangle]
 pub extern "C" fn Init_wasmer() {
     let mut wasmer_module = Module::from_existing("Wasmer");
 
-    // Declare the `Store` Ruby class.
-    {
-        let data_class = Class::from_existing("Object");
+    ruby_define! {
+        in wasmer_module
+            class (store::ruby_store) Store {
+                def_self (new) "new";
+            };
 
-        wasmer_module
-            .define_nested_class("Store", Some(&data_class))
-            .define(|this| {
-                this.def_self("new", store::ruby_store::new);
-            });
-    }
+            class (module::ruby_module) Module {
+                def_self (validate) "validate";
+                def_self (new) "new";
+                def (set_name) "name=";
+                def (get_name) "name";
+                def (custom_sections) "custom_sections";
+                def (serialize) "serialize";
+            };
 
-    // Declare the `Module` Ruby class.
-    {
-        let data_class = Class::from_existing("Object");
+            class (types) Type {
+                @const I32 = Integer::new(1);
+                @const I64 = Integer::new(2);
+                @const F32 = Integer::new(3);
+                @const F64 = Integer::new(4);
+                @const V128 = Integer::new(5);
+                @const EXTERN_REF = Integer::new(6);
+                @const FUNC_REF = Integer::new(7);
+            };
 
-        wasmer_module
-            .define_nested_class("Module", Some(&data_class))
-            .define(|this| {
-                this.def_self("validate", module::ruby_module::validate);
-                this.def_self("new", module::ruby_module::new);
-                this.def("name", module::ruby_module::get_name);
-                this.def("name=", module::ruby_module::set_name);
-                this.def("custom_sections", module::ruby_module::custom_sections);
-                this.def("serialize", module::ruby_module::serialize);
-            });
-    }
+            class (types::ruby_functiontype) FunctionType {
+                def_self (new) "new";
+                def (params) "params";
+                def (results) "results";
+            };
 
-    // Declare the `Type` class.
-    {
-        let data_class = Class::from_existing("Object");
-
-        wasmer_module
-            .define_nested_class("Type", Some(&data_class))
-            .define(|this| {
-                this.const_set("I32", &Integer::new(1));
-                this.const_set("I64", &Integer::new(2));
-                this.const_set("F32", &Integer::new(3));
-                this.const_set("F64", &Integer::new(4));
-                this.const_set("V128", &Integer::new(5));
-                this.const_set("EXTERN_REF", &Integer::new(6));
-                this.const_set("FUNC_REF", &Integer::new(7));
-            });
-    }
-
-    // Declare the `FunctionType` class.
-    {
-        let data_class = Class::from_existing("Object");
-
-        wasmer_module
-            .define_nested_class("FunctionType", Some(&data_class))
-            .define(|this| {
-                this.def_self("new", types::ruby_functiontype::new);
-                this.def("params", types::ruby_functiontype::params);
-                this.def("results", types::ruby_functiontype::results);
-            });
-    }
-
-    // Declare the `MemoryType` class.
-    {
-        let data_class = Class::from_existing("Object");
-
-        wasmer_module
-            .define_nested_class("MemoryType", Some(&data_class))
-            .define(|this| {
-                this.def_self("new", types::ruby_memorytype::new);
-                this.def("minimum", types::ruby_memorytype::minimum);
-                this.def("maximum", types::ruby_memorytype::maximum);
-                this.def("shared", types::ruby_memorytype::shared);
-            });
-    }
+            class (types::ruby_memorytype) MemoryType {
+                def_self (new) "new";
+                def (minimum) "minimum";
+                def (maximum) "maximum";
+                def (shared) "shared";
+            };
+    };
 }
