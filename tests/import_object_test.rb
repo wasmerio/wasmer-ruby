e3ceee8be8276c078bdba477523968413d192158
Ivan Enderlin <ivan@mnt.io>
2021-05-06 14:01:06 +0200

    test: Add test case for `Global` in `ImportObject`.

--- tests/import_object_test.rb
+++ tests/import_object_test.rb
@@ -37,4 +37,43 @@ class ImportObjectTest < Minitest::Test
 
     assert_equal instance.exports.add_one.(1), 2
   end
+
+  #def test_import_memory
+  #end
+
+  def test_import_global
+    store = Store.new
+    module_ = Module.new(
+      store,
+      (<<~WAST)
+        (module
+          (import "env" "global" (global $global (mut i32)))
+          (func (export "read_g") (result i32)
+            global.get $global)
+          (func (export "write_g") (param i32)
+            local.get 0
+            global.set $global))
+      WAST
+    )
+
+    global = Global.new store, Value.i32(7), true
+
+    import_object = ImportObject.new
+    import_object.register(
+      "env",
+      {
+        :global => global
+      }
+    )
+
+    instance = Instance.new module_, import_object
+
+    assert_equal instance.exports.read_g.(), 7
+
+    global.value = 153
+    assert_equal instance.exports.read_g.(), 153
+
+    instance.exports.write_g.(11)
+    assert_equal global.value, 11
+  end
 end
