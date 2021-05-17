440f224427a8d87c17d40bb8aeca48e3f08324d1
Ivan Enderlin <ivan@mnt.io>
2021-05-17 15:02:27 +0200

    doc: Move examples.

--- examples/simple.rb
+++
@@ -1,8 +0,0 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-
-require "wasmer"
-
-file = File.expand_path "simple.wasm", File.dirname(__FILE__)
-bytes = IO.read file, mode: "rb"
-instance = Wasmer::Instance.new bytes
-puts instance.exports.sum 1, 2
